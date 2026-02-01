5a907f91b324c8e4797050b930b78ae2863d22e2
Joeykmoore13 <168593468+Joeykmoore13@users.noreply.github.com>
2026-01-31 17:05:12 -0700

    Added the modals for the images and added the menu button for smaller screen sizes. Updated a little bit of css to be mobile first design.

--- prove/coolpics/script.js
+++ prove/coolpics/script.js
@@ -18,3 +18,53 @@ window.addEventListener('resize', function (event) {
     }
 }, true);
 
+const images = document.getElementById("images");
+const modal = document.querySelector('dialog');
+const modalImage = modal.querySelector('img');
+const close_button = document.getElementById("close-button");
+
+
+images.addEventListener('click', openModal);
+
+function openModal(e) {
+    console.log(e);
+    const image = e.target;
+    const src = image.getAttribute('src');
+    const alt = image.getAttribute('alt');
+
+    const full = src.replace('sm', 'full');
+
+    modalImage.src = full;
+    modalImage.alt = alt;
+    modal.showModal();
+}
+
+close_button.addEventListener('click', () => {
+    modal.close();
+});
+
+modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.close();
+    }
+});
+
+const menu_button = document.getElementById("mobile-nav");
+menu_button.addEventListener('click', displayNav);
+
+function displayNav() {
+    console.log("Ran displayNav");
+    const alt_nav = document.getElementById('alt-nav');
+    const mediaQuery = window.matchMedia("(max-width: 700px)");
+    console.log(mediaQuery);
+    if (mediaQuery.matches) {
+        if (alt_nav.hidden == true) {
+            alt_nav.hidden = false
+        }
+        else {
+            alt_nav.hidden = true
+        }
+    }
+}
+// alt-nav
+

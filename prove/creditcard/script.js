8a6646cfee3ce3e7d3fb639a5bba6ea0ecbd244a
Joeykmoore13 <168593468+Joeykmoore13@users.noreply.github.com>
2026-02-23 11:30:46 -0700

    Figured out why it wasn't checking the expiration date properly. Fixed it.

--- prove/creditcard/script.js
+++ prove/creditcard/script.js
@@ -5,8 +5,8 @@ document.getElementById("checkout").addEventListener('submit', submitPayment);
 function submitPayment(event) {
     event.preventDefault()
     let card_number = document.getElementById('card-number-input').value.trim();
-    let card_year = document.getElementById('expiration-year').value.trim();
-    let card_month = document.getElementById('expiration-month').value.trim();
+    let card_year = Number(document.getElementById('expiration-year').value.trim());
+    let card_month = Number(document.getElementById('expiration-month').value.trim());
     
     if (!/^\d{16}$/.test(card_number)) {
         alert(
@@ -17,12 +17,19 @@ function submitPayment(event) {
         alert('Card number is not valid\n');
         return
     }
-    
+
     const currentDate = new Date()
     console.log("Checking date");
-    console.log(2000 + card_year < currentDate.getFullYear() || (2000 + card_year === currentDate.getFullYear() && card_month <= (currentDate.getMonth())))
-    if (2000 + card_year < currentDate.getFullYear() || (2000 + card_year === currentDate.getFullYear() && card_month <= (currentDate.getMonth())))
+    // console.log(2000 + card_year < currentDate.getFullYear() || (2000 + card_year === currentDate.getFullYear() && card_month <= (currentDate.getMonth())))
+    // console.log(2000 + card_year < currentDate.getFullYear());
+    // console.log(2000 + card_year === currentDate.getFullYear() && card_month <= (currentDate.getMonth() + 1));
+    
+    if (2000 + card_year < currentDate.getFullYear())
     {
+        alert('Card is expired');
+        return;
+    }
+    else if (2000 + card_year === currentDate.getFullYear() && card_month <= (currentDate.getMonth() + 1)){
         alert('Card is expired');
         return;
     }
